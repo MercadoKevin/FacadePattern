1c8411de7a8ba164cc8dc35363171d320e30c954
Yohdah <Kevindalemercad@gmail.com>
2024-09-24 08:48:59 +0800

    Create Light.java

---
+++ Light.java
@@ -0,0 +1,8 @@
+public class Light implements HomeService {
+    public void turnOn() {
+        System.out.println("Lights are ON");
+    }
+    public void turnOff() {
+        System.out.println("Lights are OFF");
+    }
+}
