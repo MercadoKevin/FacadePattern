d7b882dc8ee6f905b6c22f6e548ac2e94652e698
Yohdah <Kevindalemercad@gmail.com>
2024-09-24 08:51:33 +0800

    Create AirConditioning.java

---
+++ AirConditioning.java
@@ -0,0 +1,8 @@
+public class AirConditioning implements HomeService {
+    public void turnOn() {
+        System.out.println("Air Conditioning is ON");
+    }
+    public void turnOff() {
+        System.out.println("Air Conditioning is OFF");
+    }
+}
