2b154242673fa3a3e544c507d25e0168dc776207
Yohdah <Kevindalemercad@gmail.com>
2024-09-24 08:49:27 +0800

    Create Tv.java

---
+++ Tv.java
@@ -0,0 +1,8 @@
+public class TV implements HomeService {
+    public void turnOn() {
+        System.out.println("TV is ON");
+    }
+    public void turnOff() {
+        System.out.println("TV is OFF");
+    }
+}
